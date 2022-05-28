eafc92d252517d529ea4ab45824d2d5ed90df4f9
EgorPavlovskiy <pixer.ru@mail.ru>
2022-05-28 11:13:22 +0300

    Дз 2 без доп задания

--- Lesson2/project/js/main.js
+++ Lesson2/project/js/main.js
@@ -1,38 +1,45 @@
-class ProductList{
-    constructor(container='.products'){
+class ProductList {
+    constructor(container = '.products') {
         this.container = container;
         this.goods = [];
         this._fetchProducts();//рекомендация, чтобы метод был вызван в текущем классе
         this.render();//вывод товаров на страницу
     }
-    _fetchProducts(){
+
+    _fetchProducts() {
         this.goods = [
-            {id: 1, title: 'Notebook', price: 2000},
-            {id: 2, title: 'Mouse', price: 20},
-            {id: 3, title: 'Keyboard', price: 200},
-            {id: 4, title: 'Gamepad', price: 50},
+            { id: 1, title: 'Notebook', price: 2000 },
+            { id: 2, title: 'Mouse', price: 20 },
+            { id: 3, title: 'Keyboard', price: 200 },
+            { id: 4, title: 'Gamepad', price: 50 },
         ];
     }
-    
-    render(){
+
+    getSumPrice() {
+        let sum = 0;
+        this.goods.forEach(item => {
+            sum += item.price;
+        })
+    }
+
+    render() {
         const block = document.querySelector(this.container);
-        for(let product of this.goods){
-             const item = new ProductItem(product);
-             block.insertAdjacentHTML("beforeend",item.render());
-             // block.innerHTML += item.render();
+        for (let product of this.goods) {
+            const item = new ProductItem(product);
+            block.insertAdjacentHTML("beforeend", item.render());
         }
     }
 }
 
-class ProductItem{
-    constructor(product,img='https://via.placeholder.com/200x150'){
+class ProductItem {
+    constructor(product, img = 'https://via.placeholder.com/200x150') {
         this.title = product.title;
         this.id = product.id;
         this.price = product.price;
         this.img = img;
     }
-    render(){
-           return `<div class="product-item">
+    render() {
+        return `<div class="product-item">
                 <img src="${this.img}">
                 <h3>${this.title}</h3>
                 <p>${this.price}</p>
@@ -41,25 +48,29 @@ class ProductItem{
     }
 }
 
+class Basket {
+
+    addGood() {
+
+    };
+
+    removeGood() {
+
+    };
+
+    changeGood() {
+
+    };
+
+    render() {
+
+    };
+}
+
+class ElemBasket {
+    render() {
+
+    };
+}
+
 let list = new ProductList();
-
-
-
-//const products = [
-//    {id: 1, title: 'Notebook', price: 2000},
-//    {id: 2, title: 'Mouse', price: 20},
-//    {id: 3, title: 'Keyboard', price: 200},
-//    {id: 4, title: 'Gamepad', price: 50},
-//];
-//
-//const renderProduct = (product,img='https://placehold.it/200x150') => {
-//    return `<div class="product-item">
-//                <img src="${img}">
-//                <h3>${product.title}</h3>
-//                <p>${product.price}</p>
-//                <button class="buy-btn">Купить</button>
-//            </div>`
-//};
-//const renderPage = list => document.querySelector('.products').innerHTML = list.map(item => renderProduct(item)).join('');
-//
-//renderPage(products);
