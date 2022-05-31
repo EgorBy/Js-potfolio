2cfa36dc0de765407d2f8a39e57150ebf35d0dc6
EgorPavlovskiy <pixer.ru@mail.ru>
2022-05-31 16:47:29 +0300

    Дз2 доделано

---
+++ Lesson2/project/js/main.js
@@ -0,0 +1,76 @@
+class ProductList {
+    constructor(container = '.products') {
+        this.container = container;
+        this.goods = [];
+        this._fetchProducts();//рекомендация, чтобы метод был вызван в текущем классе
+        this.render();//вывод товаров на страницу
+    }
+
+    _fetchProducts() {
+        this.goods = [
+            { id: 1, title: 'Notebook', price: 2000 },
+            { id: 2, title: 'Mouse', price: 20 },
+            { id: 3, title: 'Keyboard', price: 200 },
+            { id: 4, title: 'Gamepad', price: 50 },
+        ];
+    }
+
+    getSumPrice() {
+        let sum = 0;
+        this.goods.forEach(item => {
+            sum += item.price;
+        })
+    }
+
+    render() {
+        const block = document.querySelector(this.container);
+        for (let product of this.goods) {
+            const item = new ProductItem(product);
+            block.insertAdjacentHTML("beforeend", item.render());
+        }
+    }
+}
+
+class ProductItem {
+    constructor(product, img = 'https://via.placeholder.com/200x150') {
+        this.title = product.title;
+        this.id = product.id;
+        this.price = product.price;
+        this.img = img;
+    }
+    render() {
+        return `<div class="product-item">
+                <img src="${this.img}">
+                <h3>${this.title}</h3>
+                <p>${this.price}</p>
+                <button class="buy-btn">Купить</button>
+            </div>`
+    }
+}
+
+class Basket {
+
+    addGood() {
+
+    };
+
+    removeGood() {
+
+    };
+
+    changeGood() {
+
+    };
+
+    render() {
+
+    };
+}
+
+class ElemBasket {
+    render() {
+
+    };
+}
+
+let list = new ProductList();
