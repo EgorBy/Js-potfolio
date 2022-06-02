1c7b4b6983451f8f4475254f153189181ef310e6
EgorPavlovskiy <pixer.ru@mail.ru>
2022-06-02 17:48:12 +0300

    Готовое дз3

--- Lesson3/project/js/main.js
+++ Lesson3/project/js/main.js
@@ -16,18 +16,67 @@ const API = 'https://raw.githubusercontent.com/GeekBrainsTutorial/online-store-a
 //     xhr.send();
 // };
 
+class Basket {
+    constructor(container = '.cart-block') {
+        this.container = container;
+        this.goods = [];
+
+        this._clickBasket();
+        this._getBasketItem()
+            .then(data => {
+                this.goods = data.contents;
+                this.render();
+            });
+    }
+
+    _getBasketItem() {
+
+        return fetch(`${API}/getBasket.json`)
+            .then(result => result.json())
+            .catch(error => {
+                console.log(error);
+            });
+    }
+
+    render() {
+        const block = document.querySelector(this.container);
+        for (let product of this.goods) {
+            const productObj = new BasketItem();
+            block.insertAdjacentHTML('beforeend', productObj.render(product));
+        }
+    }
+
+    _clickBasket() {
+        document.querySelector('.btn-cart').addEventListener('click', () => {
+            document.querySelector(this.container).classList.toggle('hidden');
+        });
+    }
+}
+
+class BasketItem {
 
 
+    render(product, img = 'https://via.placeholder.com/50x50') {
+        return `<div class="cart-item" data-id="${product.id_product}">
+                <div class="product-bio">
+                <img src="${img}" alt="Some img">
+                    <h3>${product.product_name}</h3>
+                    <p>Quantity: ${product.quantity} $</p>
+                    <button class="buy-btn">Купить</button>
+                </div>
+            </div>`
+    }
+}
 
 class ProductsList {
-    constructor(container = '.products'){
+    constructor(container = '.products') {
         this.container = container;
         this.goods = [];//массив товаров из JSON документа
         this._getProducts()
             .then(data => { //data - объект js
-                 this.goods = data;
-//                 console.log(data);
-                 this.render()
+                this.goods = data;
+                //                 console.log(data);
+                this.render()
             });
     }
     // _fetchProducts(cb){
@@ -37,23 +86,23 @@ class ProductsList {
     //         cb();
     //     })
     // }
-    _getProducts(){
-      
+    _getProducts() {
+
         return fetch(`${API}/catalogData.json`)
             .then(result => result.json())
             .catch(error => {
                 console.log(error);
             });
-       
+
     }
-    calcSum(){
+    calcSum() {
         return this.allProducts.reduce((accum, item) => accum += item.price, 0);
     }
-    render(){
+    render() {
         const block = document.querySelector(this.container);
-        for (let product of this.goods){
+        for (let product of this.goods) {
             const productObj = new ProductItem(product);
-//            this.allProducts.push(productObj);
+            //            this.allProducts.push(productObj);
             block.insertAdjacentHTML('beforeend', productObj.render());
         }
 
@@ -62,13 +111,13 @@ class ProductsList {
 
 
 class ProductItem {
-    constructor(product, img = 'https://via.placeholder.com/200x150'){
+    constructor(product, img = 'https://via.placeholder.com/200x150') {
         this.title = product.product_name;
         this.price = product.price;
         this.id = product.id_product;
         this.img = img;
     }
-    render(){
+    render() {
         return `<div class="product-item" data-id="${this.id}">
                 <img src="${this.img}" alt="Some img">
                 <div class="desc">
@@ -83,3 +132,4 @@ class ProductItem {
 let list = new ProductsList();
 console.log(list.allProducts);
 
+let basketEl = new Basket();
